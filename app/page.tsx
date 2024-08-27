de973913d532c1ce4eb349833a3751dbf4b24d8f
FarwaNadeem <66826986+FarwaNadeem@users.noreply.github.com>
2024-08-27 13:24:57 +0500

    remove export

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 import Logout from "@/components/Logout";
 import { getServerSession } from "next-auth";
 
-export const AccountOption = ({
+const AccountOption = ({
   heading,
   description,
   navigateTo,
